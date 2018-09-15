1fc87279434afa6b8d64d84fc9e79bf78e8550a0
Ethan <rainshanghsu@qq.com>
2018-09-16 02:20:44 +1000

    ugly implement of the swagger doc. fuck the flask restplus doc api

--- asst02/WBEIService.py
+++ asst02/WBEIService.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, request
-from flask_restplus import Api, Resource, abort, reqparse
+from flask_restplus import Api, Resource, fields, abort
 from pymongo import MongoClient
 import json
 import requests
@@ -16,21 +16,38 @@ mongo_db = mongo_client['wbei']
 
 
 @api.route('/<collections>')
-class ImportCollection(Resource):
+class Collections(Resource):
+    @api.doc(
+        body=api.model('', {
+            'indicator_id': fields.String(description='http://api.worldbank.org/v2/indicators', example='NY.GDP.MKTP.CD'),
+        }),
+        responses={
+            200: """
+        {
+            "location" : "/<collections>/<collection_id>",
+            "collection_id" : "<collection_id>",
+            "creation_time": "2018-04-08T12:06:11Z",
+            "indicator" : "<indicator>"
+        }""",
+            201: """
+        {
+            "location" : "/<collections>/<collection_id>",
+            "collection_id" : "<collection_id>",
+            "creation_time": "2018-04-08T12:06:11Z",
+            "indicator" : "<indicator>"
+        }""",
+            400: """
+        {
+            "message": "<error msg>"
+        }"""
+        })
     def post(self, collections):
-        args = None
-        if not request.content_type or 'application/json' not in request.content_type:
-            args = json.loads(request.data)
-            if 'indicator_id' not in args:
-                abort(400, "'indicator_id' missing")
-        else:
-            parser = reqparse.RequestParser()
-            parser.add_argument('indicator_id',
-                                required=True,
-                                location='json',
-                                type=str,
-                                help='an indicator http://api.worldbank.org/v2/indicators')
-            args = parser.parse_args()
+        parser = api.parser()
+        parser.add_argument('indicator_id',
+                            required=True,
+                            location='json',
+                            type=str)
+        args = parser.parse_args()
 
         indicator_id = args['indicator_id']
         mongo_coll = mongo_db[collections]
@@ -58,30 +75,91 @@ class ImportCollection(Resource):
         while current_page <= __TOTAL_PAGE:
             response = requests.get(
                 __WBEI_DATA_URL.format(indicator_id=indicator_id, page=current_page))
+
             r_json = response.json()
-            indicator_array = r_json[1]
+            if len(r_json) < 2:
+                abort(400, r_json[0]['message'][0]['value'])
+            else:
+                indicator_array = r_json[1]
 
-            if not data:
-                one_indicator = indicator_array[0]
-                data['indicator'] = one_indicator['indicator']['id']
-                data['indicator_value'] = one_indicator['indicator']['value']
-                data['entries'] = []
+                if not data:
+                    one_indicator = indicator_array[0]
+                    data['indicator'] = one_indicator['indicator']['id']
+                    data['indicator_value'] = one_indicator['indicator']['value']
+                    data['entries'] = []
 
-            for one_indicator in indicator_array:
-                item = {
-                    'country': one_indicator['country']['value'],
-                    'date': one_indicator['date'],
-                    'value': one_indicator['value'],
-                }
-                data['entries'].append(item)
+                for one_indicator in indicator_array:
+                    item = {
+                        'country': one_indicator['country']['value'],
+                        'date': one_indicator['date'],
+                        'value': one_indicator['value'],
+                    }
+                    data['entries'].append(item)
 
             current_page += 1
 
         return mongo_coll.insert_one(data).inserted_id
 
+    @api.doc(
+        responses={
+            200: """
+            [ 
+                { 
+                    "location" : "/<collections>/<collection_id_1>", 
+                    "collection_id" : "collection_id_1",  
+                    "creation_time": "<time>",
+                    "indicator" : "<indicator>"
+                    },
+                { 
+                    "location" : "/<collections>/<collection_id_2>", 
+                    "collection_id" : "collection_id_2",  
+                    "creation_time": "<time>",
+                    "indicator" : "<indicator>"
+                },
+                ...
+                ]""",
+            400: """
+            {
+                "message": "<error msg>"
+            }"""
+        })
+    def get(self, collections):
+        mongo_coll = mongo_db[collections]
+        record = mongo_coll.find({}, {
+            "indicator": 1
+        },)
+        if record:
+            response = []
+            for doc in record:
+                o_id = doc['_id']
+                response.append(
+                    {
+                        'location': '/{}/{}'.format(collections, o_id),
+                        'collection_id': str(o_id),
+                        'creation_time': o_id.generation_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                        'indicator': doc['indicator']
+                    }
+                )
+            return response
+        else:
+            return {
+                'message': 'Collection = {} is found from the database!'.format(collections)
+            }, 400
+
 
 @api.route('/<collections>/<collection_id>')
-class DeleteCollection(Resource):
+class CollectionsCollection_id(Resource):
+    @api.doc(
+        responses={
+            200: """
+            { 
+                "message" :"Collection = <collection_id> is removed from the database!"
+            }""",
+            400: """
+            {
+                "message": "<error msg>"
+            }"""
+        })
     def delete(self, collections, collection_id):
         mongo_coll = mongo_db[collections]
         record = mongo_coll.find_one_and_delete(
